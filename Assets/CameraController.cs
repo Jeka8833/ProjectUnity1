909ce8412b33634091e6ab8c1e1b19a409290e9d
TesF0res <yura_1973_yura@mail.ru>
2020-12-31 18:26:37 +0300

    111
    111

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,33 +1,89 @@
 ﻿using UnityEngine;
+using System.Collections;
 
-public class CameraController : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
 
-    [Header("References")]
-    [Tooltip("Reference to the main camera used for the player")]
-    public AnimationManagerUI anim;
+public class CameraController : MonoBehaviour
+{
 
-    private float moveSpeed = 0.5f;
-    private float scrollSpeed = 10f;
+	public float speed = 1.5f;
 
-    float horizontalInput;
-    float verticalInput;
-    float wheelInput;
+	public Transform head;
 
-    void Update () {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
-        wheelInput = Input.GetAxis("Mouse ScrollWheel");
-    }
+	public float sensitivity = 5f; // чувствительность мыши
+	public float headMinY = -40f; // ограничение угла для головы
+	public float headMaxY = 40f;
 
-    void FixedUpdate()
-    {
-        if (Input.GetAxisRaw("Horizontal") != 0 || verticalInput != 0) {
-            transform.position += moveSpeed * new Vector3(horizontalInput, 0, verticalInput);
-            anim.SetAnimation_Run();
-        }
+	public KeyCode jumpButton = KeyCode.Space; // клавиша для прыжка
+	public float jumpForce = 10; // сила прыжка
+	public float jumpDistance = 1.2f; // расстояние от центра объекта, до поверхности
 
-        if (Input.GetAxis("Mouse ScrollWheel") != 0) {
-            transform.position += scrollSpeed * new Vector3(0, -Input.GetAxis("Mouse ScrollWheel"), 0);
-        }
-    }
+	private Vector3 direction;
+	private float h, v;
+	private int layerMask;
+	private Rigidbody body;
+	private float rotationY;
+
+	void Start()
+	{
+		body = GetComponent<Rigidbody>();
+		body.freezeRotation = true;
+		layerMask = 1 << gameObject.layer | 1 << 2;
+		layerMask = ~layerMask;
+	}
+
+	void FixedUpdate()
+	{
+		body.AddForce(direction * speed, ForceMode.VelocityChange);
+
+		// Ограничение скорости, иначе объект будет постоянно ускоряться
+		if (Mathf.Abs(body.velocity.x) > speed)
+		{
+			body.velocity = new Vector3(Mathf.Sign(body.velocity.x) * speed, body.velocity.y, body.velocity.z);
+		}
+		if (Mathf.Abs(body.velocity.z) > speed)
+		{
+			body.velocity = new Vector3(body.velocity.x, body.velocity.y, Mathf.Sign(body.velocity.z) * speed);
+		}
+	}
+
+	bool GetJump() // проверяем, есть ли коллайдер под ногами
+	{
+		RaycastHit hit;
+		Ray ray = new Ray(transform.position, Vector3.down);
+		if (Physics.Raycast(ray, out hit, jumpDistance, layerMask))
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	void Update()
+	{
+		h = Input.GetAxis("Horizontal");
+		v = Input.GetAxis("Vertical");
+
+		// управление головой (камерой)
+		float rotationX = head.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
+		rotationY += Input.GetAxis("Mouse Y") * sensitivity;
+		rotationY = Mathf.Clamp(rotationY, headMinY, headMaxY);
+		head.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
+		// вектор направления движения
+		direction = new Vector3(h, 0, v);
+		direction = head.TransformDirection(direction);
+		direction = new Vector3(direction.x, 0, direction.z);
+
+		if (Input.GetKeyDown(jumpButton) && GetJump())
+		{
+			body.velocity = new Vector2(0, jumpForce);
+		}
+	}
+
+	void OnDrawGizmosSelected() // подсветка, для визуальной настройки jumpDistance
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawRay(transform.position, Vector3.down * jumpDistance);
+	}
 }
